811d980a6f54b998caa996e3da33945b43a25976
tiendn <15717476+tiendn@users.noreply.github.com>
2024-09-20 00:23:13 +0700

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { getChainOptions } from "@terra-money/wallet-controller";
+import { WalletProvider } from "@terra-money/wallet-provider";
+import "tippy.js/dist/tippy.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "config/lang";
+import { BRIDGE } from "config/constants";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "index.scss";
+import ScrollToTop from "app/ScrollToTop";
+import InitNetworks from "app/InitNetworks";
+import InitWallet from "app/InitWallet";
+import InitTheme from "app/InitTheme";
+import ElectronVersion from "app/ElectronVersion";
+import App from "app/App";
+
+const connectorOpts = { bridge: BRIDGE };
+
+getChainOptions().then((chainOptions) =>
+	render(
+		<StrictMode>
+			<RecoilRoot>
+				<BrowserRouter>
+					<ScrollToTop />
+					<WalletProvider {...chainOptions} connectorOpts={connectorOpts}>
+						<InitNetworks>
+							<InitWallet>
+								<InitTheme />
+								<ElectronVersion />
+								<App />
+							</InitWallet>
+						</InitNetworks>
+					</WalletProvider>
+				</BrowserRouter>
+			</RecoilRoot>
+		</StrictMode>,
+		document.getElementById("station")
+	)
+);
